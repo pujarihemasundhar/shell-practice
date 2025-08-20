23346c8316c3bab143df8b2c2be450b08dfc7edf
Sundhar <pujarihemasundhar@gmail.com>
2025-08-20 17:15:29 +0530

    Conversation shell script file addded

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hi Putin, how are you?"
+echo "Putin: Hi Trump, Fine, Hope you are doing well"
+echo "Trump: Ceasefire the war"
+echo "Putin: We have some demands"
+echo "Trump: We will meet at alaska and will discuss"
