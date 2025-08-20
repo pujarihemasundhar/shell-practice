59412f47b0f9fff205f8b68bb582fffdaea0cd74
Sundhar <pujarihemasundhar@gmail.com>
2025-08-20 17:22:57 +0530

    varaibales file added

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=trump
+person2=putin
+
+echo "$person1: Hi $person2, how are you?"
+echo "$person2: Hi $person1, Fine, Hope you are doing well"
+echo "$person1: Ceasefire the war"
+echo "$person2: We have some demands"
+echo "$person1: We will meet at alaska and will discuss"
