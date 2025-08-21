67afca6078048b3e2b7d07dcef77abf1efec8d60
Sundhar <pujarihemasundhar@gmail.com>
2025-08-21 15:31:56 +0530

    conditions added if mysql got installed

--- install-command.sh
+++ install-command.sh
@@ -5,8 +5,21 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "ERROR:: please run this script with root acccess"
+    exit 1
 else
     echo "you are running with root access"
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0]
+then
+    echo "MySQL is not installed.....GOING to INSTALL..."
+    dnf install mysql -y
+    if [ $? -eq 0]
+    then 
+        echo "Installing MySQL is .......SUCCESS"
+    else
+        echo "Installing MySQL is ........FAILURE"
+else
+    echo "MySQL is already installed.NOTHING TO DO"
