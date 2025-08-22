08ccc8a0ce34a8b8080660d8ed300bdb21ae47a8
Sundhar <pujarihemasundhar@gmail.com>
2025-08-22 10:50:16 +0530

    created variable for the colors in the colors.sh script file

--- colors.sh
+++ colors.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo -e "\e[31m Hello Colors \e[0m"
+#echo -e "\e[31m Hello Colors \e[0m"
 
-echo "Hi, Good Morning"
+#echo "Hi, Good Morning"
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ $userid ne 0 ]
+then
+    echo -e " $R ERROR:: Please run the script with root user"
+else
+    echo "You are running with root user"
