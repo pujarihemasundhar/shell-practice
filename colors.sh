33c3a3137b35c64d3e4398fc71053c38709556a8
Sundhar <pujarihemasundhar@gmail.com>
2025-08-22 11:06:37 +0530

    validate function added to the colors.sh script file

--- colors.sh
+++ colors.sh
@@ -15,6 +15,27 @@ N="\e[0m"
 if [ $userid -ne 0 ]
 then
     echo -e " $R ERROR:: Please run the script with root user"
+    exit 1
 else
     echo "You are running with root user"
+fi
+
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+        echo "Installing $2 is..........SUCCCESS"
+    else
+        echo "Installing $2 is...........FAILURE"
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "MySQL is not installed.....Going to install"
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo "MySQL is already installed..... NOTHING TO DO"
 fi
